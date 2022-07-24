cb6f2a04a01f3b146d0f84153509c9413fec4e04
Admin <Admin@DESKTOP-MNBM418>
2022-07-24 15:44:56 +0530

    new commit

--- MAVENSDET36/src/test/java/mavenClasses/MavenTest.java
+++ MAVENSDET36/src/test/java/mavenClasses/MavenTest.java
@@ -6,12 +6,12 @@ import org.testng.annotations.Test;
 public class MavenTest {
 	@Test
 public void display() {
-		Reporter.log("TestCase");
+		Reporter.log("TestCase",true);
 		//this is for branch
 	
 }
 	@Test
 	public void display1() {
-		Reporter.log("Testcase is pass");
+		Reporter.log("Testcase is pass",true);
 	}
 }
